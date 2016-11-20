70ae3707e6629a0420c1d13ecaa6527755bfef64
wahyusoft <wahyusoft@yahoo.com>
2016-11-20 22:43:57 +0700

    changing base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ci_CURD/';
+$config['base_url']	= 'http://localhost/CI_CURD/';
 
 /*
 |--------------------------------------------------------------------------
